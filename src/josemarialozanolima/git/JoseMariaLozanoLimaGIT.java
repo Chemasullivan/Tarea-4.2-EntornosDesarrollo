943bf1c09dd16c4bbdcef0fe4f722ca0c2ec194b
Chema y Bea <Chema y Bea@DESKTOP-G78JONV>
2023-03-01 20:03:30 +0100

    Valor = 2 se ha cambiado el valor de 1 a 2

--- src/josemarialozanolima/git/JoseMariaLozanoLimaGIT.java
+++ src/josemarialozanolima/git/JoseMariaLozanoLimaGIT.java
@@ -7,7 +7,7 @@ public class JoseMariaLozanoLimaGIT {
     
     public static void main(String[] args) {
         
-        int valor1 = 1; //este es el valor que ira cabiado en cada verión.
+        int valor1 = 2; //este es el valor que ira cabiado en cada verión.
         int valor2 = 6;
         int operacion = valor1 + valor2;
         System.out.println(operacion);
