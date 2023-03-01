f6e1838e5f4735f1c3e40130ce2a4f1a4859e603
Chema y Bea <Chema y Bea@DESKTOP-G78JONV>
2023-03-01 20:02:43 +0100

    Version original. Valor=1

---
+++ src/josemarialozanolima/git/JoseMariaLozanoLimaGIT.java
@@ -0,0 +1,17 @@
+
+package josemarialozanolima.git;
+
+
+public class JoseMariaLozanoLimaGIT {
+
+    
+    public static void main(String[] args) {
+        
+        int valor1 = 1; //este es el valor que ira cabiado en cada verión.
+        int valor2 = 6;
+        int operacion = valor1 + valor2;
+        System.out.println(operacion);
+        
+    }
+    
+}
