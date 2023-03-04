49046ac7f307ac9bf8ce7f5eaa2a6c3ae9535616
Chema y Bea <Chema y Bea@DESKTOP-G78JONV>
2023-03-04 16:48:56 +0100

    cambio, incorporamos un comentario.

--- src/josemarialozanolima/git/JoseMariaLozanoLimaGIT.java
+++ src/josemarialozanolima/git/JoseMariaLozanoLimaGIT.java
@@ -8,7 +8,7 @@ public class JoseMariaLozanoLimaGIT {
     public static void main(String[] args) {
         
         
-        int valor2 = 6;
+        int valor2 = 6; //hacemos cambio con un comentario solamente
         ClaseNueva valornuevo = new ClaseNueva ();
         valornuevo.setCambio(4);
         int operacion = valornuevo.getCambio () + valor2;
